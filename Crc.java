a8968c054d7088f8814d4850b07cadf99d25a2de
Siddharth Pugalia <sidpug@gmail.com>
2021-01-11 10:29:30 +0530

    Update Crc.java

--- Crc.java
+++ Crc.java
@@ -1,97 +1,56 @@
-import java.io.*;
-class Crc
+import java.util.Scanner;
+class CRC
 {
-public static void main(String args[]) throws IOException
+static String datastream;
+static String generator = “10001000000100001”;
+public static void main(String args[])
 {
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-int[ ] data;
-int[ ]div;
-int[ ]divisor;
-int[ ]rem;
-int[ ] crc;
-int data_bits, divisor_bits, tot_length;
-System.out.println("Enter number of data bits : ");
-data_bits=Integer.parseInt(br.readLine());
-data=new int[data_bits];
-System.out.println("Enter data bits : ");
-for(int i=0; i<data_bits; i++)
-data[i]=Integer.parseInt(br.readLine());
-System.out.println("Enter number of bits in divisor : ");
-divisor_bits=Integer.parseInt(br.readLine());
-divisor=new int[divisor_bits];
-System.out.println("Enter Divisor bits : ");
-for(int i=0; i<divisor_bits; i++)
-divisor[i]=Integer.parseInt(br.readLine());
-/* System.out.print("Data bits are : ");
-for(int i=0; i< data_bits; i++)
-System.out.print(data[i]);
-System.out.println();
-System.out.print("divisor bits are : ");
-for(int i=0; i< divisor_bits; i++)
-System.out.print(divisor[i]);
-System.out.println();
-*/
-tot_length=data_bits+divisor_bits-1;
-div=new int[tot_length];
-rem=new int[tot_length];
-crc=new int[tot_length];
-/*------------------ CRC GENERATION-----------------------*/
+Scanner sc = new Scanner(System.in);
+System.out.println(“---At the Sender---\n Enter data stream:”);
+String datastream = sc.nextLine();
+int datalen = datastream.length();
+int genlen = generator.length();
+int data[] = new int[datalen + genlen – 1];
+int codeword = new int[datalen + genlen – 1];
+int div[] = new int[generator.length()];
+for(int i=0;i<datastream.length();i++)
+data[i] = Integer.parseInt(datastream.charAt(i)+””);
+for(int i=0;i<generator.length();i++)
+div[i] = Integer.parseInt(generator.charAt(i)+””);
+codeword = calculateCrc(data,div,datalen);
+System.out.println(“The CRC(Final Codeword) code is:”);
+for(int i=0;i<datastream.length();i++)
+codeword[i] = Integer.parseInt(datastream.charAt(i)+””);
 for(int i=0;i<data.length;i++)
-div[i]=data[i];
-System.out.print("Dividend (after appending 0's) are : ");
-for(int i=0; i< div.length; i++)
-System.out.print(div[i]);
-System.out.println();
-for(int j=0; j<div.length; j++){
-rem[j] = div[j];
-}
-rem=divide(div, divisor, rem);
-for(int i=0;i<div.length;i++) //append dividend and remainder
+System.out.print(codeword[i]);
+System.out.println(“\n”);
+System.out.print(“---At the Receiver---\n Enter Received codeword:”);
+datastream = sc.nextLine();
+data = new int[datastream.length() + generator.length() – 1];
+for(int i=0;i<datastream.length();i++)
+data[i] = Integer.parseInt(datastream.charAt(i)+””);
+codeword = calculateCrc(data,div,datalen);
+boolean valid = true;
+for(int i=0;i<codeword.length;i++)
+if(codeword[i]==1)
 {
-crc[i]=(div[i]^rem[i]);
-}
-System.out.println();
-System.out.println("CRC code : ");
-for(int i=0;i<crc.length;i++)
-System.out.print(crc[i]);
-/*-------------------ERROR DETECTION---------------------*/
-System.out.println();
-System.out.println("Enter CRC code of "+tot_length+" bits : ");
-for(int i=0; i<crc.length; i++)
-crc[i]=Integer.parseInt(br.readLine());
-/* System.out.print("crc bits are : ");
-or(int i=0; i< crc.length; i++)
-System.out.print(crc[i]);
-System.out.println();
-*/
-for(int j=0; j<crc.length; j++){
-rem[j] = crc[j];
-}
-rem=divide(crc, divisor, rem);
-for(int i=0; i< rem.length; i++)
-{
-if(rem[i]!=0)
-{
-System.out.println("Error: The data with CRC has data bit errors");
+valid = false;
 break;
 }
-if(i==rem.length-1)
-System.out.println("No Error: The data wth CRC is proper");
+if(valid==true)
+System.out.println(“Data stream is valid. No error occurred”);
+else
+System.out.println(“Data stream is invalid. CRC error occurred”);
+sc.close();
 }
-}
-static int[] divide(int div[],int divisor[], int rem[])
+Public static int[] calculateCRC(int[] divrem, int[] divisor, int len)
 {
-int cur=0;
-while(true)
+for(int i=0;i<len;i++)
 {
-for(int i=0;i<divisor.length;i++)
-rem[cur+i]=(rem[cur+i]^divisor[i]);
-while(rem[cur]==0 && cur!=rem.length-1)
-cur++;
-//Check if we have reached the end of division
-if((rem.length-cur)<divisor.length)
-break;
+if(divrem[i]==1)
+for(int j=0;j<divisor.length;j++)
+divrem[i+j]^=divisor[j];
 }
-return rem;
+return divrem;
 }
 }
